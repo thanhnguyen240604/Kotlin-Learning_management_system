aec00920c5e6723ff39c6afbb24634ee16f66504
minhtank22 <tan.nguyendn14042004@hcmut.edu.vn>
2024-12-04 10:59:14 +0700

    modify Subject model: using subject_code as Id

--- src/main/kotlin/com/be/kotlin/grade/repository/SubjectRepository.kt
+++ src/main/kotlin/com/be/kotlin/grade/repository/SubjectRepository.kt
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface SubjectRepository : JpaRepository<Subject, Long>{}
+interface SubjectRepository : JpaRepository<Subject, String>
