aaceb07779202fd839837a7872859b1b0457926f
Công Thành <nguyencongthanh2406@gmail.com>
2024-12-05 23:00:19 +0700

    Add handler exception for Grade Service

--- src/main/kotlin/com/be/kotlin/grade/exception/ErrorCode.kt
+++ src/main/kotlin/com/be/kotlin/grade/exception/ErrorCode.kt
@@ -3,6 +3,11 @@ package com.be.kotlin.grade.exception
 enum class ErrorCode(val code: Int, val message: String) {
     //Resource not match
     PASSWORD_INVALID(400, "Password invalid"),
+    STUDY_ID_INVALID(400,"StudyId cannot be null"),
+    GRADE_ID_INVALID(400,"GradeId cannot be null"),
+    SCORE_INVALID(400, "Score must be between 0 and 10"),
+    WEIGHT_LIMIT_INVALID(400, "Weight exceeds limit"),
+    GRADE_NOT_MATCH_INVALID(400, "Grade does not belong to the specified study"),
 
     //Unauthenticated
     UNAUTHENTICATED(401, "Unauthenticated"),
@@ -11,6 +16,7 @@ enum class ErrorCode(val code: Int, val message: String) {
     USER_NOT_FOUND(404, "User not found"),
     SUBJECT_NOT_FOUND(404,"Subject not found"),
     STUDY_NOT_FOUND(404,"Study not found"),
+    GRADE_NOT_FOUND(404,"Grade not found"),
 
     //Resource already existed
     USER_EXISTED(409, "User existed"),
--- src/main/kotlin/com/be/kotlin/grade/model/Study.kt
+++ src/main/kotlin/com/be/kotlin/grade/model/Study.kt
@@ -10,15 +10,15 @@ data class Study(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id", referencedColumnName = "student_id", nullable = false)
     val student: Student? = Student(),
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "subject_id", referencedColumnName = "id", nullable = false)
     val subject: Subject? = Subject(),
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "class_id", referencedColumnName = "id", nullable = false)
     val studyClass: Class? = Class(),
 
@@ -28,6 +28,6 @@ data class Study(
     var score: Float? = null,
 
     @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-    @JoinColumn(name = "study_id")  // Chỉ định cột khóa ngoại trong bảng Grade
+    @JoinColumn(name = "study_id")
     var gradesList: MutableList<Grade> = mutableListOf()
 )
